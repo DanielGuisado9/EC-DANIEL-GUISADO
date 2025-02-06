cbc2ab3eaab05450dd449eb4244e2e7ff945ea3d
DanielGuisado9 <dguisadonunez@adaits.es>
2025-02-06 19:33:29 +0100

    prueba

---
+++ La-Barbiere-Proyecto/src/app/components/home/home-page/home-page.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../layout/navbar/navbar.component';
+import { FooterComponent } from '../../layout/footer/footer.component';
+import { HeroSectionComponent } from '../hero-section/hero-section.component';
+import { CommonModule } from '@angular/common';
+import { ContentListComponent } from '../content-list/content-list.component'; // Importamos ContentList
+import { ReservationsService, Haircut } from '../../../services/reservations.service';
+import { RankingCortesComponent } from '../ranking-cortes/ranking-cortes.component';
+
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent, HeroSectionComponent, CommonModule, ContentListComponent, RankingCortesComponent], // Incluimos ContentListComponent
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent {
+  popularHaircuts: Haircut[] = [];
+  allHaircuts: Haircut[] = [];
+
+  constructor(private reservationsService: ReservationsService) {}
+
+  ngOnInit(): void {
+   
+    this.allHaircuts = this.reservationsService.getRandomHaircuts(6); // Obtener todos los datos
+  }
+}
